f87f0a4837dc24e45a0d07c832e8cdec19553867
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-08-08 10:14:14 -0300

    first commit

---
+++ src/main/java/com/lscud/curso/exemploProjeto/ExemploProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.lscud.curso.exemploProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lscud/curso/exemploProjeto/ExemploProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lscud.curso.exemploProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
